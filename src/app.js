90c27b146cf01b5effce57fbb9435d509ffdfe30
Nicki Vance <nicki.vance@intel.com>
2022-09-03 19:00:13 +0100

    Init repo.

---
+++ src/app.js
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>The appy app app.</h1>;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import { App } from "./app";
+
+const app = ReactDOM.createRoot(document.getElementById('app'));
+app.render(<App/>);
